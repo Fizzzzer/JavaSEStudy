c6f1da6d1509207289950779229919827b40d52b
Fizzer_Mac <doraemonma503@gmail.com>
2018-07-26 15:28:44 +0800

    First Commit

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Doraemon on 2018/7/26.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
